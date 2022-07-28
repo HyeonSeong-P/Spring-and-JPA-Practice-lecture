3a491a907b73cd5445d7f20252fedd6d500ec1d3
phs <gustjd0808@pusan.ac.kr>
2022-07-28 13:14:08 +0900

    회원 수정, 조회 API [API 개발 기본] / 조회용 샘플 데이터 입력 [API 개발 고급 - 준비]

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -2,14 +2,15 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -17,6 +18,56 @@ import javax.validation.constraints.NotEmpty;
 public class MemberApiController {
     private final MemberService memberService;
 
+    /**
+     * 조회 V1: 응답 값으로 엔티티를 직접 외부에 노출한다.
+     * 문제점
+     * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+     * - 기본적으로 엔티티의 모든 값이 노출된다.
+     * - 응답 스펙을 맞추기 위해 로직이 추가된다. (@JsonIngnore, 별도의 뷰 로직 등등)
+     * - 실무에선 같은 엔티티에 대해 API가 용도에 따라 다양하게 만들어지는데, 한 엔티티에 각각의 API를 위한 프레젠테이션 응답 로직을 담긴 어렵다.
+     * - 엔티티가 변경되면 API 스펙이 변한다.
+     * - 추가로 컬렉션을 직접 반환하면 향후 API 스펙을 변경하기 어렵다.(별도의 Result 클래스 생성으로 해결)
+     *
+     * 결론
+     * - API 응답 스펙에 맞추어 별도의 DTO를 반환한다.
+     */
+    // 조회 V1: 안 좋은 버전, 모든 엔티티가 노출, @JsonIgnore -> api가 이거 하나가 아니기 때문에 이 방법은 최악.
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
+
+    /**
+     * 조회 V2: 응답 값으로 엔티티가 아닌 별도의 DTO를 반환한다.
+     *
+     * - 엔티티가 변해도 API 스펙이 변경되지 않는다.
+     * - 추가로 Result 클래스로 컬렉션을 감싸서 향후 필요한 필드를 추가할 수 있다.
+     * @return
+     */
+    @GetMapping("/api/v2/members")
+    public Result membersV2(){
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        /**
+         * 이렇게 감싸지 않으면 json 배열 형식으로만 나가기 때문에 향후 필요한 필드를 추가하기 위해선
+         * 이런 식으로 컬렉션을 감싸는 게 좋다
+         */
+        return new Result(collect);
+    }
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
     /**
      * @RequestBody 어노테이션은 json으로 온 body를 파라미터(여기선 Member)로 매핑해줌
      * 등록 V1: 요청 값으로 Member 엔티티를 직접 받는다.
@@ -51,6 +102,37 @@ public class MemberApiController {
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
+
+    /**
+     * 여기선 회원 정보를 부분 업데이트를 한다
+     * PUT 방식을 사용했는데, PUT은 전체 업데이트를 할 때 사용하는 것이 맞다.
+     * 부분 업데이트를 하려면 PATCH를 사용하거나 POST를 사용하는 것이 맞다!!!
+     *
+     */
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request
+    ){
+        /**
+         * 커맨드(변경성 메서드)와 쿼리를 분리한다, 유지보수에 용이하다
+         */
+        memberService.update(id,request.getName()); // 커맨드, 업데이트 후 바로 멤버 객체를 반환하지 않고 다시 조회한다
+        Member findMember = memberService.findOne(id); // 쿼리
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse{
+        private Long id;
+        private String name;
+    }
+    @Data
+    static class UpdateMemberRequest{
+        private String name;
+    }
+
 
     @Data
     static class CreateMemberRequest{
